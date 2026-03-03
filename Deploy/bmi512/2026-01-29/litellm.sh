59573294836f5f3be2b3f39216e78137bc1a7e68
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2026-03-03 10:42:39 -0500

    moved

--- Deploy/bmi512/2026-01-29/litellm.sh
+++
@@ -1,9 +0,0 @@
-docker run -d \
-  --name litellm \
-  --restart always \
-  -p 4000:4000 \
-  -e AZURE_API_KEY="AZURE_API_KEY" \
-  -e AZURE_API_BASE="https://bmi512sp2026.cognitiveservices.azure.com" \
-  -e AZURE_API_VERSION="2025-04-01-preview" \
-  ghcr.io/berriai/litellm:main-latest \
-  --model azure/gpt-5.2
--- Deploy/bmi512/2026-01-30/ufw-web-allow-dev-deny.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo ufw allow 80
-sudo ufw allow 443
-sudo ufw deny 3000
-sudo ufw deny 5858
